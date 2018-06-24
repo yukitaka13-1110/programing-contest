d982a2ae9970deea2960eeca50d879404d6f4a5a
長井孝幸 <somaruyo_kanoko@yahoo.co.jp>
2018-06-24 17:04:09 +0900

    C++用のマクロテンプレートの追加

---
+++ cpp_macro_template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define REPR(i, n) for(int i = n;i >= 0;i--)
+#define FOR(i, m, n) for(int i = m;i < n;i++)
+#define FORR(i, m, n) for(int i = m;i >= n;i--)
+#define SORT(v, n) sort(v, v+n);
+#define VSORT(v) sort(v.begin(), v.end());
+#define llong long long
+#define pb(a) push_back(a)
+#define INF 999999999
+using namespace std;
+typedef pair<int, int> P;
+typedef pair<llong, llong> LP;
+typedef pair<int, P> PP;
+typedef pair<llong, LP> LPP;
+
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+
+int main(){
+
+}
